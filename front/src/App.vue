ed932835b14ec50786e722751a0e89019888b65e
김솔배 <sol4854@ekay.co.kr>
2024-04-05 15:37:47 +0900

    feat : Vue 추가, 게시글 작성 컴포넌트 추가

---
+++ front/src/App.vue
@@ -0,0 +1,19 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/write">피드 작성</RouterLink>
+      </nav>
+  </header>
+
+  <RouterView />
+</template>
+
+<style>
+
+</style>
---
+++ front/src/views/WriteView.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {ref} from "vue";
+import axios from "axios";
+
+const title = ref("")
+const content = ref("")
+
+const write = function () {
+  axios.post('http://localhost:8080/feeds', {
+    title: title.value,
+    content: content.value
+  })
+}
+</script>
+
+<template>
+  <div>
+    <el-input v-model="title" placeholder="제목을 입력해주세요"/>
+  </div>
+
+  <div class="mt-2">
+    <el-input v-model="content" type="textarea" rows="15"/>
+  </div>
+
+  <el-button type="primary" @click="write()">피드 작성</el-button>
+</template>
+
+<style>
+
+</style>
