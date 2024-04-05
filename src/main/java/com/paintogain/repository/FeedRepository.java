f9ee006cabb283ed174476b1861962ee1e9c71b7
김솔배 <sol4854@ekay.co.kr>
2024-04-05 10:40:38 +0900

    remove cache

--- src/main/java/com/paintogain/repository/FeedRepository.java
+++
@@ -1,7 +0,0 @@
-package com.paintogain.repository;
-
-import com.paintogain.domain.Feed;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FeedRepository extends JpaRepository<Feed, Long> {
-}
