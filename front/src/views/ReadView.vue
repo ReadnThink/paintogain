7dcee4b8bc71e7f809c62aa23e6e2119a6e193f3
김솔배 <sol4854@ekay.co.kr>
2024-04-08 18:26:15 +0900

    feat : 글 수정 페이지 구현

--- front/src/views/ReadView.vue
+++ front/src/views/ReadView.vue
@@ -1,19 +1,24 @@
 <script setup lang="ts">
 import { onMounted, ref } from 'vue'
 import axios from 'axios'
+import router from '@/router'
 
 const props = defineProps({
   feedId: {
     type: [Number, String],
     required: true
-  }
-})
+  },
+});
 
 const feed = ref({
   id: 0,
   title: "",
   content: "",
 })
+
+const moveToEdit = () => {
+  router.push({name: "edit", params: {feedId: props.feedId}})
+}
 
 onMounted(() => {
   axios.get(`/my-backend-api/feeds/${props.feedId}`).then(response => {
@@ -25,4 +30,6 @@ onMounted(() => {
 <template>
   <h2>{{ feed.title }}</h2>
   <div>{{ feed.content }}</div>
+
+  <el-button type="warning" @click="moveToEdit()">수정</el-button>
 </template>
