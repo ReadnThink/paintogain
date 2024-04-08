d375049830077f5c2cedecde0d1a9fa9f83963cb
김솔배 <sol4854@ekay.co.kr>
2024-04-08 18:10:13 +0900

    feat : 글 한건 조회 화면 구현

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -1,22 +1,34 @@
 <script setup lang="ts">
 import axios from 'axios'
 import { ref } from 'vue'
+import { useRouter } from 'vue-router'
 
-const feeds = ref([]);
+const router = useRouter()
+
+const feeds = ref([])
 axios.get('/my-backend-api/feeds?page=1&size=5')
-.then(response => {
-  console.log(response)
-  response.data.forEach((r: any) => {
-    feeds.value.push(r);
+  .then(response => {
+    console.log(response)
+    response.data.forEach((r: any) => {
+      feeds.value.push(r)
+    })
   })
-})
+
+const moveToRead = () => {
+  router.push({ name: 'read' })
+}
 </script>
 
 <template>
   <ul>
-    <li v-for="feed in feeds" :key="feeds.id">
-      <div>{{ feed.title }}</div>
-      <div>{{ feed.content }}</div>
+    <li v-for="feed in feeds" :key="feeds.id" @click="moveToRead()">
+      <div>
+        <router-link :to="{ name: 'read', params: {feedId : feed.id} }" >{{ feed.title }} </router-link>
+      </div>
+
+      <div>
+        {{ feed.content }}
+      </div>
 
     </li>
   </ul>
---
+++ front/src/views/ReadView.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { onMounted, ref } from 'vue'
+import axios from 'axios'
+
+const props = defineProps({
+  feedId: {
+    type: [Number, String],
+    required: true
+  }
+})
+
+const feed = ref({
+  id: 0,
+  title: "",
+  content: "",
+})
+
+onMounted(() => {
+  axios.get(`/my-backend-api/feeds/${props.feedId}`).then(response => {
+      feed.value = response.data;
+    });
+});
+</script>
+
+<template>
+  <h2>{{ feed.title }}</h2>
+  <div>{{ feed.content }}</div>
+</template>
